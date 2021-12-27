7cf5cbb1432873142d979c80eaeb87ff969423d3
Anton Vasilev <ittony2020@gmail.com>
2021-12-27 04:06:13 +0300

    create hometask for lesson-2

--- lesson_02/hometask_02.1.py
+++ lesson_02/hometask_02.1.py
@@ -1,6 +1,6 @@
 # Task 1
 
-first_list = [1, 2.3, 'a', True, 3.23333, 2, 2, 'sdfg', 234, 'asdf']
+first_list = [1, 2.3, 'a', True, 3.23333, 2, 2, 'sdfg', 234, 'asdf', [1, 2, 3, 4], {1, 'ssdfsd', 234}]
 j = 0
 i = 0
 while i < len(first_list):
