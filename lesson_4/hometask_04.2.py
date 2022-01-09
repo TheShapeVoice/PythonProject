3fc055fe55708f3239db0dbd37ff80168e0de769
Anton Vasilev <ittony2020@gmail.com>
2022-01-10 00:59:06 +0300

    Выполненное ДЗ

--- lesson_4/hometask_04.2.py
+++ lesson_4/hometask_04.2.py
@@ -1,4 +1,4 @@
-# Task 2 Подсмотрел не сам решил. Пытаюсь разобраться в том как работает
+# Task 2 Подсмотрел не сам решил. Пытаюсь разобраться в том, как работает
 
 start_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 task2 = [start_list[num] for num in range(1, len(start_list)) if start_list[num] > start_list[num - 1]]
--- lesson_4/hometask_04.7.py
+++ lesson_4/hometask_04.7.py
@@ -1,1 +1,13 @@
 # Task 7
+
+from math import factorial
+
+n = int(input("Введите число: "))
+def fact():
+    for i in range(0, n + 1):
+        a = factorial(i)
+        yield f'{i}! = {a}' # подсмотрел дял читаемости по красивее
+
+for el in fact():
+    print(el)
+
